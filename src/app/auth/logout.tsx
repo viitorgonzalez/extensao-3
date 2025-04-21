909a71321c4229346c031dbe29a61c5899798151
viitorgonzalez <pastor123.vs@gmail.com>
2025-04-21 10:55:15 -0300

    testing logout

--- src/app/auth/logout.tsx
+++ src/app/auth/logout.tsx
@@ -1,31 +1,20 @@
 // app/auth/logout.ts
 'use client'
 
-import { redirect } from "next/navigation"
 import { supabase } from "../../supabase/config"
 
 export const logout = async () => {
   try {
     const { data: { session: beforeSession }, error: beforeError } = await supabase.auth.getSession()
-
+    
     if (beforeSession) {
       console.log("Usuário está logado:", beforeSession.user.email)
     } else {
       console.log("Usuário não está logado", beforeError)
     }
-
-    await supabase.auth.signOut({ scope: 'local' })
-    redirect('/login')
-    // Verifica novamente após logout
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { data: { session: afterSession }, error: afterError } = await supabase.auth.getSession()
-
-    if (!afterSession) {
-      console.log("Logout bem-sucedido: usuário não está mais logado.")
-    } else {
-      console.log("Algo deu errado: usuário ainda logado:", afterSession.user.email)
-    }
     
+    await supabase.auth.signOut({ scope: 'local' })
+
   } catch (error) {
     console.error('Erro ao sair:', error)
   }
