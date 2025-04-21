fa4262064571ad504ff3c42b9882c51fbaf2fc0b
viitorgonzalez <pastor123.vs@gmail.com>
2025-04-20 23:46:28 -0300

    testing logout

--- src/app/api/logout/Route.ts
+++ src/app/api/logout/Route.ts
@@ -1,9 +1,9 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export async function POST() {
-  const supabase = createRouteHandlerClient({ cookies })
-  await supabase.auth.signOut()
-  return NextResponse.json({ success: true })
+  const supabase = createRouteHandlerClient({ cookies });
+  await supabase.auth.signOut();
+  return NextResponse.json({ success: true });
 }
